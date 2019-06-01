f4254de6fa96c84ad89283dbfdd8481904fed2d6
chrisnager <cnager@gmail.com>
2019-06-01 10:42:58 -0400

    Update metadata, hide logs

--- index.js
+++ index.js
@@ -5,23 +5,22 @@ const GIPHY_API_KEY = 'I3Gl6RcFBwkIL0UC3IQb61op0S0Eeax8';
 let items = [];
 
 module.exports = withUiHook(async ({ payload }) => {
-  await axios(`http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=${GIPHY_API_KEY}&limit=5`)
+  await axios(`http://api.giphy.com/v1/gifs/search?q=pokemon&api_key=${GIPHY_API_KEY}&limit=1`)
     .then(({ data: { data } }) => {
-      console.log(data);
+      // console.log(data);
       items = data;
     })
-    .catch(error => console.log({ error }));
+    .catch(error => console.error({ error }));
 
   // console.log({ payload });
-  console.log('outside', items[0].images);
+  // console.log('outside', items[0].images);
 
   return htm`
     <Page>
+      <Button>Load GIF</Button>
       <UL>
         ${items && items.map(item => htm`<LI><Img src="${item.images.original.url}" /></LI>`)}
       </UL>
-      <H1>Let's see: ${count}</H1>
-      <Button>Action</Button>
     </Page>
   `;
 });
