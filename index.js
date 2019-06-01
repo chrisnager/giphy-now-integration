2e76ca76cf7020b71ab73e45c2d93217aeab60b6
chrisnager <cnager@gmail.com>
2019-06-01 12:35:48 -0400

    Update layout

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ module.exports = withUiHook(async ({ payload }) => {
 
   if (action === 'submit') store.searchTerm = clientState.searchTerm;
 
-  await axios(`http://api.giphy.com/v1/gifs/search?q=${clientState.searchTerm}&api_key=${GIPHY_API_KEY}&limit=3`)
+  await axios(`http://api.giphy.com/v1/gifs/search?q=${clientState.searchTerm}&api_key=${GIPHY_API_KEY}&limit=8`)
     .then(({ data: { data } }) => {
       // console.log(data);
       items = data;
@@ -18,24 +18,32 @@ module.exports = withUiHook(async ({ payload }) => {
     .catch(error => console.error({ error }));
 
   // console.log({ payload });
-  // console.log('outside', items[0].images);
+  // console.log('outside', items[2].images);
 
   return htm`
     <Page>
-      <Container>
-        <Input label="Search for GIFs" placeholder="cat" name="searchTerm" value="${clientState.searchTerm || ''}" />
-      </Container>
-      <Container>
+      <Box marginBottom="1rem">
+        <Input label="Search GIFs" placeholder="cat" name="searchTerm" value="${clientState.searchTerm || ''}" />
+      </Box>
+      <Box marginBottom="1rem">
         <Button>Submit</Button>
-      </Container>
-      <Container>
-        <UL>
-          ${items && items.map(item => htm`<LI><Img src="${item.images.original.url}" /></LI>`)}
-        </UL>
-      </Container>
-      <Container>
-        Created by <Link href="http://chrisnager.com" target="_blank">Chris Nager</Link>
-      </Container>
+      </Box>
+        ${
+          items.length === 0
+            ? ''
+            : htm`<Box marginBottom="1rem" padding="0.25rem" display="flex" flexWrap="wrap" border="1px solid">
+          ${items.map(
+            item =>
+              htm`<Box width="25%" height="200px" overflow="hidden" border="0.25rem solid #fafafa" display="flex" alignItems="center" backgroundColor="#000">
+                <Img width="100%" src="${item.images.original.url}" />
+              </Box>`,
+          )}
+        </Box>`
+        }
+      <AutoRefresh timeout=${3000} />
+      <Box>
+        Created by <Link href="http://chrisnager.com" target="_blank">Chris Nager</Link> for the <Link href="https://zeit.co/hackathon" target="_blank">ZEIT Hackathon</Link>.
+      </Box>
     </Page>
   `;
 });
