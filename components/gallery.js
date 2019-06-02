8030166e434d58db597ed71a3149db681fd90c42
chrisnager <cnager@gmail.com>
2019-06-01 20:21:11 -0400

    Update gallery border

--- components/gallery.js
+++ components/gallery.js
@@ -3,7 +3,8 @@ const { htm } = require('@zeit/integration-utils');
 const Gallery = ({ items }) => htm`
   <Box
     minHeight="310px"
-    border="1px solid"
+    border="1px solid #e1e1e1"
+    border-radius="5px"
     padding="0.25rem"
     display="flex"
     flexWrap="wrap"
@@ -17,7 +18,7 @@ const Gallery = ({ items }) => htm`
               <Box
                 width="25%"
                 height="150px"
-                border="0.25rem solid #fafafa"
+                border="0.25rem solid #fff"
                 display="flex"
                 alignItems="center"
                 overflow="hidden"
